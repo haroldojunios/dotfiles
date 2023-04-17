4c27493d58c2919d306eb54ab78dfa024a6e37aa
Haroldo Júnio <haroldojunios@gmail.com>
2023-04-16 21:17:52 -0300

    Add instalation script for Ubuntu

---
+++ .chezmoitemplates/install_ubuntu.sh
@@ -0,0 +1,204 @@
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+sudo apt update
+sudo apt upgrade -y
+
+NEEDS_UPDATE=
+
+# code repo
+if ! [ -f /etc/apt/sources.list.d/vscode.list ]; then
+  sudo apt install -y wget gpg apt-transport-https
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
+  sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  rm -f packages.microsoft.gpg
+  NEEDS_UPDATE=1
+fi
+
+# alacritty repo
+if ! grep -q "^deb .*aslatter/ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+  sudo add-apt-repository ppa:aslatter/ppa -y
+  NEEDS_UPDATE=1
+fi
+
+# firefox repo
+if ! grep -q "^deb .*mozillateam/ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+  sudo add-apt-repository ppa:mozillateam/ppa -y
+
+  if command -v snap &>/dev/null; then
+    if snap list | grep firefox &>/dev/null; then
+      sudo snap remove firefox
+    fi
+  fi
+
+  NEEDS_UPDATE=1
+  sudo bash -c "cat >/etc/apt/preferences.d/mozillateamppa" <<EOF
+Package: firefox*
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 501
+EOF
+fi
+
+# kubuntu backports repo
+if ! grep -q "^deb .*kubuntu-ppa/backports" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+  sudo add-apt-repository ppa:kubuntu-ppa/backports -y
+  sudo apt update && sudo apt full-upgrade -y
+  NEEDS_UPDATE=
+fi
+
+if [ -n $NEEDS_UPDATE ]; then
+  sudo apt update
+fi
+
+# desktop enviroment
+dePackageList="
+ark \
+dolphin \
+filelight \
+gwenview \
+kate \
+kcalc \
+kde-config-screenlocker \
+kde-config-sddm \
+kde-spectacle \
+konsole \
+kwin-addons \
+okular \
+plasma-browser-integration \
+plasma-desktop \
+plasma-disks \
+plasma-firewall \
+plasma-framework \
+plasma-integration \
+plasma-nm \
+plasma-pa \
+plasma-systemmonitor \
+plasma-thunderbolt \
+plasma-widgets-addons \
+plasma-workspace \
+powerdevil \
+sddm \
+xorg \
+"
+
+# applets
+appletsPackageList="
+cmake \
+extra-cmake-modules \
+g++ \
+gettext \
+libkdecorations2-dev \
+libkf5configwidgets-dev \
+libkf5declarative-dev \
+libkf5plasma-dev \
+libkf5plasma-dev \
+libkf5wayland-dev \
+libqt5x11extras5-dev \
+libsm-dev \
+libxcb-randr0-dev \
+make \
+plasma-workspace-dev \
+qtbase5-dev \
+qtdeclarative5-dev \
+"
+
+# packages
+packageList=(
+  age
+  alacritty
+  bat
+  calibre
+  clang
+  cmake
+  code
+  conky-all
+  crudini
+  curl
+  exa
+  ffmpeg
+  fish
+  firefox
+  git
+  gparted
+  imagemagick
+  jmtpfs
+  jq
+  keepassxc
+  make
+  mpv
+  nano
+  ninja-build
+  numlockx
+  p7zip
+  python-is-python3
+  python3
+  python3-venv
+  sshfs
+  systemd-zram-generator
+  texlive-fonts-recommended
+  texlive-lang-portuguese
+  texlive-latex-base
+  texlive-latex-extra
+  texlive-latex-recommended
+  texlive-pictures
+  texlive-pstricks
+  texlive-science
+  texlive-xetex
+  tmux
+  ufw
+  unzip
+  xclip
+  zip
+)
+
+if ! { [ -d /usr/share/plasma/plasmoids/org.kde.windowbuttons ] && [ -d /usr/share/plasma/plasmoids/org.kde.windowappmenu ]; }; then
+  packageList=(
+    "${packageList[@]}"
+    "$appletsPackageList"
+  )
+fi
+
+if [ -d "/sys/class/power_supply" ]; then
+  packageList=(
+    "${packageList[@]}"
+    tlp
+    tlp-rdw
+  )
+fi
+
+for package in "${packageList[@]}"; do
+  if ! dpkg -s "$package" &>/dev/null; then
+    sudo apt install -y "$package" || echo "${RED}Package ${BLUE}$package ${RED}not found!${NC}"
+  fi
+done
+
+if ! command -v bat &>/dev/null; then
+  mkdir -p ~/.local/bin
+  ln -s /usr/bin/batcat $HOME/.local/bin/bat
+fi
+
+if ! command -v starship &>/dev/null; then
+  curl -sS https://starship.rs/install.sh | sh -s -- -y
+fi
+
+if ! systemctl list-unit-files --state=enabled | grep ufw &>/dev/null; then
+  sudo systemctl enable --now ufw
+fi
+
+if [ -d "/sys/class/power_supply" ]; then
+  if ! systemctl list-unit-files --state=enabled | grep tlp &>/dev/null; then
+    sudo systemctl enable --now tlp
+  fi
+fi
+
+if ! dpkg -s "onlyoffice-desktopeditors" &>/dev/null; then
+  TEMP_FOLDER=$(mktemp -d)
+  wget -O "$TEMP_FOLDER/onlyoffice.deb" -q "https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors_amd64.deb"
+  sudo apt install -y "$TEMP_FOLDER/onlyoffice.deb"
+  rm -rf "$TEMP_FOLDER"
+fi
+
+sudo apt autoremove -y
+sudo apt clean -y
---
+++ .chezmoitemplates/style.sh
@@ -0,0 +1,101 @@
+TEMP_FOLDER=$(mktemp -d)
+
+# theme
+if ! [ -d /usr/share/themes/sweet ]; then
+  sudo mkdir -p /usr/share/icons
+  git -C "$TEMP_FOLDER" clone --depth 1 -b nova https://github.com/EliverLara/sweet
+  (
+    cd "$TEMP_FOLDER/sweet/kde"
+    mkdir -p aurorae/themes
+    mv aurorae/Sweet-Dark aurorae/themes/Sweet-Dark
+    mv aurorae/Sweet-Dark-transparent aurorae/themes/Sweet-Dark-transparent
+    rm aurorae/.shade.svg
+    mv colorschemes color-schemes
+    mkdir -p plasma/look-and-feelnano $__fish_config_dir/fish_plugins
+    mv look-and-feel plasma/look-and-feel/Sweet
+    mv sddm sddm-Sweet
+    mkdir -p sddm/themes
+    mv sddm-Sweet sddm/themes/Sweet
+
+    # cursors
+    sudo rm -r /usr/share/icons/Sweet-cursors
+    sudo mv cursors/Sweet-cursors /usr/share/icons
+
+    # kvantum
+    sudo cp -r Kvantum /usr/share
+
+    # GTK
+    sudo mkdir -p /usr/share/themes/sweet
+    sudo cp -a * /usr/share/themes/sweet
+
+    # KDE theme
+    sudo cp -r aurorae /usr/share
+    sudo cp -r color-schemes /usr/share
+    sudo cp -r konsole /usr/share
+    sudo cp -r plasma /usr/share
+    sudo cp -r sddm /usr/share
+  )
+
+  sudo mkdir -p /usr/share/plasma/desktoptheme/Sweet
+  git -C "$TEMP_FOLDER" clone --depth 1 https://github.com/EliverLara/sweet-kde
+  sudo cp -r "$TEMP_FOLDER/sweet-kde"/* /usr/share/plasma/desktoptheme/Sweet
+
+  # blur
+  git -C "$TEMP_FOLDER" clone --depth 1 https://github.com/esjeon/kwin-forceblur
+  sudo mkdir -p /usr/share/kwin/scripts/forceblur
+  sudo cp -r "$TEMP_FOLDER/kwin-forceblur/"{contents/,LICENSE,image.png,metadata.desktop} /usr/share/kwin/scripts/forceblur/
+  sudo install -Dm644 "$TEMP_FOLDER/kwin-forceblur/metadata.desktop" /usr/share/kservices5/kwin-script-forceblur.desktop
+
+  # apply config
+  lookandfeeltool -a Sweet
+  sudo crudini --set /etc/sddm.conf Theme Current Sweet
+  sudo mkdir -p /var/lib/AccountsService/icons
+  sudo cp "$SCRIPT_FOLDER/assets/avatar.png" "/var/lib/AccountsService/icons/$USER"
+
+  # killall plasmashell && kstart plasmashell
+fi
+
+# icons
+if ! [ -d /usr/share/icons/candy-icons ]; then
+  sudo mkdir -p /usr/share/icons
+  sudo git -C /usr/share/icons clone --depth 1 https://github.com/EliverLara/candy-icons.git
+fi
+
+{ command -v plasma-changeicons &>/dev/null && plasma-changeicons candy-icons &>/dev/null; } ||
+  { [ -x /usr/lib/x86_64-linux-gnu/libexec/plasma-changeicons ] && /usr/lib/x86_64-linux-gnu/libexec/plasma-changeicons candy-icons &>/dev/null; }
+
+# fonts
+if ! [ -d /usr/share/fonts/FiraCode ]; then
+  sudo mkdir -p /usr/share/fonts/FiraCode
+  wget -O "$TEMP_FOLDER/firacode.zip" -q "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraCode.zip"
+  sudo unzip -q "$TEMP_FOLDER/firacode.zip" -d /usr/share/fonts/FiraCode
+  sudo rm -f /usr/share/fonts/FiraCode/*windows*
+  fc-cache -f
+fi
+
+# applets
+if ! [ -d /usr/share/plasma/plasmoids/org.kde.windowbuttons ]; then
+  git -C "$TEMP_FOLDER" clone --depth 1 https://github.com/psifidotos/applet-window-buttons.git
+  (
+    cd "$TEMP_FOLDER/applet-window-buttons"
+    sh ./install.sh
+  )
+fi
+
+if ! [ -d /usr/share/plasma/plasmoids/org.kde.windowappmenu ]; then
+  git -C "$TEMP_FOLDER" clone --depth 1 https://github.com/psifidotos/applet-window-appmenu.git
+  (
+    cd "$TEMP_FOLDER/applet-window-appmenu"
+    sh ./install.sh
+  )
+fi
+
+if ! [ -d /usr/share/plasma/plasmoids/org.kde.windowtitle ]; then
+  git -C "$TEMP_FOLDER" clone --depth 1 https://github.com/psifidotos/applet-window-title.git
+  (
+    cd "$TEMP_FOLDER/applet-window-title"
+    sudo kpackagetool5 --global --install .
+  )
+fi
+
+rm -rf "$TEMP_FOLDER"
