c3646ad77b7b62becc7d8fc521ed935f47505263
Haroldo Júnio <haroldojunios@gmail.com>
2023-04-22 15:16:51 -0300

    Add termux install

---
+++ .chezmoitemplates/install_termux.sh
@@ -0,0 +1,42 @@
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+apt update
+apt upgrade -y
+
+# its pointless repo
+if ! [ -f $PREFIX/etc/apt/sources.list.d/pointless.list ]; then
+  curl -sL https://its-pointless.github.io/setup-pointless-repo.sh | bash
+fi
+
+packageList=(
+  age
+  bat
+  clang
+  cmake
+  exa
+  ffmpeg
+  fish
+  git
+  imagemagick
+  jq
+  make
+  nano
+  ninja
+  p7zip
+  python
+  scipy
+  tmux
+  unzip
+  zip
+)
+
+for package in "${packageList[@]}"; do
+  if ! dpkg -s $package &>/dev/null; then
+    apt install -y $package || echo -e "${RED}Package ${BLUE}$package ${RED}not found!${NC}"
+  fi
+done
+
+apt autoremove -y
+apt clean
