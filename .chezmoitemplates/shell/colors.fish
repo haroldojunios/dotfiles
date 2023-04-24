1b6877efd08e2a0fe12eff41e4e34d836fe86ea3
Haroldo Júnio <haroldojunios@gmail.com>
2023-04-24 20:11:34 -0300

    Add color variables to shell

---
+++ .chezmoitemplates/shell/colors.fish
@@ -0,0 +1,42 @@
+# color aliases
+# foreground
+set FMT_BLACK '\033[30m'  # black
+set FMT_RED '\033[31m'    # red
+set FMT_GREEN '\033[32m'  # green
+set FMT_YELLOW '\033[33m' # yellow
+set FMT_BLUE '\033[34m'   # blue
+set FMT_PURPLE '\033[35m' # purple
+set FMT_CYAN '\033[36m'   # cyan
+set FMT_WHITE '\033[37m'  # white
+
+# background
+set FMT_BLACK_BG '\033[40m'  # black
+set FMT_RED_BG '\033[41m'    # red
+set FMT_GREEN_BG '\033[42m'  # green
+set FMT_YELLOW_BG '\033[43m' # yellow
+set FMT_BLUE_BG '\033[44m'   # blue
+set FMT_PURPLE_BG '\033[45m' # purple
+set FMT_CYAN_BG '\033[46m'   # cyan
+set FMT_WHITE_BG '\033[47m'  # white
+
+# high intensity background
+set FMT_BLACK_HIBG '\033[0;100m'  # black
+set FMT_RED_HIBG '\033[0;101m'    # red
+set FMT_GREEN_HIBG '\033[0;102m'  # green
+set FMT_YELLOW_HIBG '\033[0;103m' # yellow
+set FMT_BLUE_HIBG '\033[0;104m'   # blue
+set FMT_PURPLE_HIBG '\033[0;105m' # purple
+set FMT_CYAN_HIBG '\033[0;106m'   # cyan
+set FMT_WHITE_HIBG '\033[0;107m'  # white
+
+# other
+set FMT_RESET '\033[0m'      # reset
+set FMT_BOLD '\033[1m'       # bold
+set FMT_FAINT '\033[2m'      # faint
+set FMT_IT '\033[3m'         # italic
+set FMT_UNDERLINE '\033[4m'  # underline
+set FMT_BLINK '\033[5m'      # blink
+set FMT_BLINK_FAST '\033[6m' # blink fast
+set FMT_REV '\033[7m'        # reverse
+set FMT_INVISIBLE '\033[8m'  # invisible
+set FMT_STRIKE '\033[9m'     # strike-through
