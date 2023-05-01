24e0a7f5443d51bcf1a07f0ad4641d5b15a3698b
Haroldo Júnio <haroldojunios@gmail.com>
2023-04-30 23:05:16 -0300

    Add setup cron script

---
+++ .chezmoitemplates/setup_cron.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CRONTAB="
+@reboot fish -c 'fisher update >/dev/null'
+{{ if eq .osid "android" }}
+0 * * * * find $HOME/storage/shared/Android/media/com.whatsapp/WhatsApp/Databases/ -type f ! -name msgstore.db.crypt14 -delete >/dev/null 2>&1
+{{ end }}
+"
+
+if command -v crontab &>/dev/null; then
+  if ! crontab -l 2>/dev/null | grep -q "$CRONTAB"; then
+    echo $CRONTAB | crontab -
+  fi
+fi
