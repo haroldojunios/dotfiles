18a5c2b803ebfd269ecadd7f366395b5fdcc5d05
Haroldo Júnio <haroldojunios@gmail.com>
2023-08-07 20:01:58 -0300

    Add desktop environment to rasp

--- .chezmoitemplates/install_rasp.sh
+++ .chezmoitemplates/install_rasp.sh
@@ -8,6 +8,14 @@ NC='\033[0m' # No Color
 sudo apt-get update
 sudo apt-get full-upgrade -y
 
+dePackageList=(
+  xserver-xorg
+  raspberrypi-ui-mods
+  rpi-chromium-mods
+  lxterminal
+  python3-tk
+)
+
 packageList=(
   age
   bat
@@ -24,30 +32,32 @@ packageList=(
   jq
   make
   micro
+  mosquitto
+  mosquitto-clients
   nano
   ninja-build
   openssh-server
   p7zip
   python-is-python3
-  python3
+  python3-dev
+  python3-pip
+  python3-rpi.gpio
   python3-venv
   shfmt
   sshfs
-  texlive-fonts-recommended
-  texlive-lang-portuguese
-  texlive-latex-base
-  texlive-latex-extra
-  texlive-latex-recommended
-  texlive-pictures
-  texlive-pstricks
-  texlive-science
-  texlive-xetex
   tmux
   unzip
   wget
   xclip
   zip
 )
+
+{{ if .installDE }}
+packageList=(
+  "${dePackageList[@]}"
+  "${packageList[@]}"
+)
+{{ end }}
 
 for package in "${packageList[@]}"; do
   if ! dpkg -s $package &>/dev/null; then
