654ce9b7ea0aa2ac8c06d4787a0ecd18a77ee721
Haroldo Júnio <haroldojunios@gmail.com>
2023-12-05 10:42:18 -0300

    Update windows install to use only choco

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,29 @@
 Set-ExecutionPolicy RemoteSigned
-iwr -useb get.scoop.sh | iex
 
-scoop install main/chezmoi
+if (!(Get-Command choco -errorAction SilentlyContinue))
+{
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+if (!(Get-Command age -errorAction SilentlyContinue))
+{
+  choco install age.portable -y
+}
+
+if (!(Get-Command gsudo -errorAction SilentlyContinue))
+{
+  choco install gsudo -y
+}
+
+if (!(Get-Command chezmoi -errorAction SilentlyContinue))
+{
+  choco install chezmoi -y
+}
+
+
+if (gsudo config | Select-String -Pattern "Explicit") {
+  gsudo config CacheMode Auto
+  gsudo config CacheDuration "00:20:00"
+}
 
 chezmoi init --apply haroldojunios
