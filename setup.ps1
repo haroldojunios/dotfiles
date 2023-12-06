abba02bb0631716c76138c6a26891de0d78ca59b
Haroldo Júnio <haroldojunios@gmail.com>
2023-12-06 11:13:57 -0300

    Add choco to path on setup

--- setup.ps1
+++ setup.ps1
@@ -3,6 +3,11 @@ Set-ExecutionPolicy RemoteSigned
 if (!(Get-Command choco -errorAction SilentlyContinue))
 {
   Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+  # add choco to path
+  $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
+  Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+  refreshenv
 }
 
 if (!(Get-Command age -errorAction SilentlyContinue))
