23c2fe9a4e724d588af8e33e57b2578dd717a1bd
Haroldo Júnio <haroldojunios@gmail.com>
2023-12-11 10:52:52 -0300

    Refresh env of win packages

--- setup.ps1
+++ setup.ps1
@@ -3,11 +3,6 @@ Set-ExecutionPolicy RemoteSigned
 if (!(Get-Command choco -errorAction SilentlyContinue))
 {
   Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-  # add choco to path
-  $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
-  Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-  refreshenv
 }
 
 if (!(Get-Command age -errorAction SilentlyContinue))
@@ -24,6 +19,11 @@ if (!(Get-Command chezmoi -errorAction SilentlyContinue))
 {
   choco install chezmoi -y
 }
+
+# update choco modules
+$env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+refreshenv | Out-Null
 
 
 if (gsudo config | Select-String -Pattern "Explicit") {
