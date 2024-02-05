2328ac7b29d48034bc89798a08a224da67c3d6d7
Haroldo Júnio <haroldojunios@gmail.com>
2024-02-05 10:27:40 -0300

    Update "update-db" script

--- .chezmoitemplates/shell/chezmoi.sh
+++ .chezmoitemplates/shell/chezmoi.sh
@@ -1,10 +1,11 @@
 # shellcheck shell=bash
 # chezmoi
 alias cm=chezmoi
-alias cma='chezmoi add'
+alias cmad='chezmoi add'
+alias cmap='chezmoi apply'
 alias cmae='chezmoi add --encrypt'
 alias cmra='chezmoi re-add'
 alias cmc='cd $(chezmoi source-path)'
-alias cmu='chezmoi update --init --apply'
+alias cmu='{{ if ne .chezmoi.os "android" }}sudo -v && {{ end }}chezmoi update --init --apply'
 alias cme='chezmoi edit'
 alias cmet='chezmoi execute-template'
