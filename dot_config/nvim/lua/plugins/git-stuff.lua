e6f4c929f229fa6bd659178f06d2097a2a64e7ea
Haroldo Júnio <haroldojunios@gmail.com>
2024-03-03 21:15:44 -0300

    Add lazygit to nvim

--- dot_config/nvim/lua/plugins/git-stuff.lua
+++ dot_config/nvim/lua/plugins/git-stuff.lua
@@ -5,10 +5,26 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup()
+      require("gitsigns").setup({
+        current_line_blame = true
+      })
 
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
       vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
     end
+  },
+  {
+    {
+      "kdheepak/lazygit.nvim",
+      dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim"
+      },
+      config = function()
+        require("telescope").load_extension("lazygit")
+
+        vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>')
+      end,
+    },
   }
 }
