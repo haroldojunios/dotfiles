d668c6c68a1f24afdc573f0aaa41ddfaae30ecee
Haroldo Júnio <haroldojunios@gmail.com>
2024-03-03 23:07:34 -0300

    Add tabs to nvim

---
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,38 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = "*",
+  dependencies = {
+    'nvim-tree/nvim-web-devicons'
+  },
+  config = function()
+    local groups = require('bufferline.groups')
+
+    require("bufferline").setup({
+      options = {
+        separator_style = "slant",
+        numbers = "ordinal",
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local icon = level:match("error") and " " or " "
+          return " " .. icon .. count
+        end,
+        groups = {
+          items = {
+            require('bufferline.groups').builtin.pinned:with({ icon = "" }),
+            groups.builtin.ungrouped -- other items
+          }
+        },
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = function()
+              return vim.fn.getcwd()
+            end,
+            highlight = "Directory",
+            separator = true
+          }
+        }
+      }
+    })
+  end
+}
