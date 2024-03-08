86efd40c1a52a2b1475ac335c44e199248c50e93
Haroldo Júnio <haroldojunios@gmail.com>
2024-03-07 22:27:46 -0300

    Add `codeium` to nvim

--- dot_config/nvim/lua/plugins/completions.lua
+++ dot_config/nvim/lua/plugins/completions.lua
@@ -10,6 +10,17 @@ return {
     },
   },
   {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+      "onsails/lspkind.nvim",
+    },
+    config = function()
+      require("codeium").setup()
+    end,
+  },
+  {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
@@ -34,10 +45,19 @@ return {
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "luasnip" }, -- For luasnip users.
+          { name = "luasnip" },
+          { name = "codeium" },
         }, {
           { name = "buffer" },
         }),
+        formatting = {
+          format = require("lspkind").cmp_format({
+            mode = "symbol",
+            maxwidth = 50,
+            ellipsis_char = "...",
+            symbol_map = { Codeium = "" },
+          }),
+        },
       })
     end,
   },
