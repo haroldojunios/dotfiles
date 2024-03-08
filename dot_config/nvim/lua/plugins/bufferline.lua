062291ca19be576ab3425f8356d4eb968848e0d6
Haroldo Júnio <haroldojunios@gmail.com>
2024-03-07 22:27:30 -0300

    Add commands to change tabs on nvim

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -27,6 +27,11 @@ return {
             groups.builtin.ungrouped, -- other items
           },
         },
+        hover = {
+          enabled = true,
+          delay = 200,
+          reveal = { "close" },
+        },
         offsets = {
           {
             filetype = "NvimTree",
@@ -39,5 +44,74 @@ return {
         },
       },
     })
+
+    vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { silent = true })
+    vim.keymap.set(
+      "n",
+      "<S-Tab>",
+      ":BufferLineCyclePrev<CR>",
+      { silent = true }
+    )
+
+    vim.keymap.set(
+      "n",
+      "<leader>1",
+      ":BufferLineGoToBuffer 1<CR>",
+      { silent = true }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>2",
+      ":BufferLineGoToBuffer 2<CR>",
+      { silent = true }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>3",
+      ":BufferLineGoToBuffer 3<CR>",
+      { silent = true }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>4",
+      ":BufferLineGoToBuffer 4<CR>",
+      { silent = true }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>5",
+      ":BufferLineGoToBuffer 5<CR>",
+      { silent = true }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>6",
+      ":BufferLineGoToBuffer 6<CR>",
+      { silent = true }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>7",
+      ":BufferLineGoToBuffer 7<CR>",
+      { silent = true }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>8",
+      ":BufferLineGoToBuffer 8<CR>",
+      { silent = true }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>9",
+      ":BufferLineGoToBuffer 9<CR>",
+      { silent = true }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>0",
+      ":BufferLineGoToBuffer -1<CR>",
+      { silent = true }
+    )
   end,
 }
