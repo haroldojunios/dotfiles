7ac6ef7b40265e754324d6fd1d4847d391b9f4fa
Haroldo Júnio <haroldojunios@gmail.com>
2024-03-14 23:31:05 -0300

    Update vimtex pdf viewer

--- dot_config/exact_nvim/lua/plugins/vimtex.lua
+++ dot_config/exact_nvim/lua/plugins/vimtex.lua
@@ -26,7 +26,9 @@ return {
         "Token not allowed in a PDF string",
       }
       vim.g.vimtex_view_general_viewer = "okular"
-      vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+      vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+      vim.g.tex_flavor = "latex"
+      vim.g.vimtex_compiler_progname = "nvr"
     end,
   },
 }
