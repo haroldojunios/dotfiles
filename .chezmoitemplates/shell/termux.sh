e2b23d6757b62adb55f95256c77ffe42f698a7ef
Haroldo Júnio <haroldojunios@gmail.com>
2024-03-31 22:26:36 -0300

    Fix error when there is no package to remove

--- .chezmoitemplates/shell/termux.sh
+++ .chezmoitemplates/shell/termux.sh
@@ -4,7 +4,7 @@ alias i='apt install -y'
 alias r='apt remove -y'
 alias u='apt update && apt upgrade --fix-missing -y'
 alias s='apt search'
-alias cl='dpkg --list | grep "^rc" | cut -d " " -f 3 | xargs dpkg --purge && apt autoclean -y && apt autoremove -y'
+alias cl='dpkg --list | grep "^rc" | cut -d " " -f 3 | xargs -r dpkg --purge && apt autoclean -y && apt autoremove -y'
 
 # termux api
 alias tp='termux-camera-photo storage/pictures/$(date "+%Y%m%d_%H%M%S").jpg'
