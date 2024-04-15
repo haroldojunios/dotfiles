e1bbceb736279e5dab3f483e0cedb9208c5797aa
Haroldo Júnio <haroldojunios@gmail.com>
2024-04-15 17:42:40 -0300

    Not refresh externals on edit/add/etc

--- .chezmoitemplates/shell/chezmoi.sh
+++ .chezmoitemplates/shell/chezmoi.sh
@@ -1,11 +1,12 @@
 # shellcheck shell=bash
 # chezmoi
 alias cm=chezmoi
-alias cma='chezmoi add'
-alias cmae='chezmoi add --encrypt'
+alias cma='chezmoi --refresh-externals=never add'
+alias cmae='chezmoi --refresh-externals=never add --encrypt'
 alias cmap='chezmoi apply'
-alias cmra='chezmoi re-add'
+alias cmra='chezmoi --refresh-externals=never re-add'
 alias cmc='cd $(chezmoi source-path)'
 alias cmu='{{ if ne .chezmoi.os "android" }}sudo -v && {{ end }}chezmoi update --init --apply'
-alias cme='chezmoi edit'
-alias cmet='chezmoi execute-template'
+alias cme='chezmoi --refresh-externals=never edit'
+alias cmea='chezmoi --refresh-externals=never edit --apply'
+alias cmet='chezmoi --refresh-externals=never execute-template'
