aebe9afc1b97ba3e1739794aa5a1b82d0a0a5665
Haroldo Júnio <haroldojunios@gmail.com>
2024-04-25 13:36:35 -0300

    Auto git fetch on background

--- dot_config/fish/conf.d/git_auto_fetch.fish
+++ dot_config/fish/conf.d/git_auto_fetch.fish
@@ -1,6 +1,8 @@
 function git_auto_fetch --on-variable PWD \
     --description "git fetch automatically wherever inside a git repository"
     if git rev-parse --is-inside-work-tree &>/dev/null
-        git fetch origin 2>/dev/null || :
+        begin
+            git fetch origin 2>/dev/null & || :
+        end
     end
 end
