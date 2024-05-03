1be0cbe561dd730e33d89e7e6324c8d116f689c6
Haroldo Júnio <haroldojunios@gmail.com>
2024-05-03 18:45:42 -0300

    Use 2 spaces as tab for markdown

--- dot_config/nvim/exact_lua/filetype.lua
+++ dot_config/nvim/exact_lua/filetype.lua
@@ -26,3 +26,11 @@ vim.filetype.add({
     end,
   },
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "md",
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+  end,
+})
