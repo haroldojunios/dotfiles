339da6de148186d22e59c145c6919b76686153ef
Haroldo Júnio <haroldojunios@gmail.com>
2024-07-10 16:38:15 -0300

    Update nvim toggleterm with virt-column

--- dot_config/nvim/exact_lua/exact_plugins/terminal.lua
+++ dot_config/nvim/exact_lua/exact_plugins/terminal.lua
@@ -1,19 +1,18 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  config = function()
-    require("toggleterm").setup({
-      direction = "float",
-      open_mapping = [[<C-'>]],
-      float_opts = {
-        border = "curved",
-      },
-      on_open = function(t)
-        vim.wo[t.window].cc = ""
-        if package.loaded["virt-column"] then
-          require("virt-column").setup_buffer({ virtcolumn = "" })
-        end
-      end,
-    })
-  end,
+  opts = {
+    direction = "float",
+    open_mapping = [[<C-'>]],
+    auto_scroll = false,
+    float_opts = {
+      border = "curved",
+    },
+    on_open = function(t)
+      vim.wo[t.window].cc = ""
+      if package.loaded["virt-column"] then
+        require("virt-column").setup_buffer({ virtcolumn = "" })
+      end
+    end,
+  },
 }
