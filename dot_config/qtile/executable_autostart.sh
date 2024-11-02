e47f14c1f93df990889f8690c314bd3abfc86ca0
Haroldo Júnio <haroldojunios@gmail.com>
2024-11-02 20:13:00 -0300

    Remove unused config files

--- dot_config/qtile/executable_autostart.sh
+++
@@ -1,9 +0,0 @@
-feh --bg-fill --no-fehbg ~/.local/share/wallpapers/Shani.png &
-
-picom --config ~/.config/picom/picom.conf &
-
-command -v dex &>/dev/null && dex -a &
-
-command -v udiskie &>/dev/null && udiskie &
-
-pulseaudio --start
