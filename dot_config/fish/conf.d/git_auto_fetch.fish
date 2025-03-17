acb05c5cfa0a5dfa3bac9db5daab6b8c8c451d41
Haroldo Júnio <haroldojunios@gmail.com>
2025-03-17 11:31:02 -0300

    Fix error with auto git fetch on fish 4.0

--- dot_config/fish/conf.d/git_auto_fetch.fish
+++ dot_config/fish/conf.d/git_auto_fetch.fish
@@ -2,7 +2,7 @@ function git_auto_fetch --on-variable PWD \
     --description "git fetch automatically wherever inside a git repository"
     if git rev-parse --is-inside-work-tree &>/dev/null
         begin
-            git fetch origin 2>/dev/null & || :
-        end
+            git fetch origin 2>/dev/null || :
+        end &
     end
 end
